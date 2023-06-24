00c0c1fec039e69c0266f51266a8e9b58860f86b
Satyam Chaudhary <chaudharysatyam313@gmail.com>
2023-06-25 01:17:38 +0530

    c++ revison

---
+++ input_output.cpp
@@ -0,0 +1,21 @@
+//to include all the libraries at once instead of only iostream we can use <bits/stdc++.h>
+#include<bits/stdc++.h>
+using namespace std; //makes program use std for all the program
+
+int main(){
+
+    // std::cout << "Hello World" << "\n";
+    // cout << "Hello World" << endl;
+
+   //both std::endl and \n do the same thing
+
+   int x;
+   cin >> x;
+   cout << "Value of x is: " << x << endl;
+
+   int y , z;
+   cin >> y >> z;
+   cout << "Value of y is:" << y << endl << "Value of z is" << z << endl; 
+
+    return 0;
+}
